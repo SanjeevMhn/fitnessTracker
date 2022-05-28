bfb92d28a6bd83a041099aad88572dca50131653
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2022-05-28 23:50:06 +0545

    [FIXED]: fixed the workout viewer modal

--- src/components/WorkoutViewer.vue
+++ src/components/WorkoutViewer.vue
@@ -5,13 +5,15 @@
         data(){
             return{
                 workoutView: false,
+								workoutIndex: this.workoutIndex,
             }
-        }
+        },
+				
     }
 </script>
 
 <template>
-    <div class="workout-viewer">
+    <div class="workout-viewer" v-if="workouts.length !== 0">
         <div class="close-btn">
             <button type="button" class="icon-container" @click="$emit('hideWorkout',workoutView)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
@@ -22,6 +24,20 @@
         <h2 class="workout-viewer__title">{{this.workouts[this.workoutIndex].title}}</h2>
         <p class="workout-viewer__desc">
 						{{this.workouts[this.workoutIndex].desc}}
+				</p>
+    </div>
+
+		<div class="workout-viewer" v-else>
+        <div class="close-btn">
+            <button type="button" class="icon-container" @click="$emit('hideWorkout',workoutView)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </button>
+        </div>
+        <h2 class="workout-viewer__title">{{this.workouts}}</h2>
+        <p class="workout-viewer__desc">
+						{{this.workouts}}
 				</p>
     </div>
 </template>
